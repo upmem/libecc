933cb9369d6a11108d2989730ec08f7c2d3d23b3
Massimiliano Bartoli <mbartoli@upmem.com>
2022-03-10 18:18:32 +0100

    SEC-11: move indentation to spaces
    Signed-off-by: Massimiliano Bartoli <mbartoli@upmem.com>

--- src/DPU/ecdsa_host.c
+++ src/DPU/ecdsa_host.c
@@ -33,24 +33,24 @@ extern int usleep (__useconds_t __useconds);
 
 static void prepare_data(mram_t *area)
 {
-	int fdbin;
-	area->dpu_policy = DPU_POLICY_VERIFY_AND_JUMP;
-	/* Copying signature data */
-	memcpy(area->sig_data, public_key, sizeof(public_key));
-	memcpy(&area->sig_data[sizeof(public_key)], calculated_hash, sizeof(calculated_hash));
-	memcpy(&area->sig_data[sizeof(public_key) + sizeof(calculated_hash)], signature, sizeof(signature));
+    int fdbin;
+    area->dpu_policy = DPU_POLICY_VERIFY_AND_JUMP;
+    /* Copying signature data */
+    memcpy(area->sig_data, public_key, sizeof(public_key));
+    memcpy(&area->sig_data[sizeof(public_key)], calculated_hash, sizeof(calculated_hash));
+    memcpy(&area->sig_data[sizeof(public_key) + sizeof(calculated_hash)], signature, sizeof(signature));
 #ifdef SIG_KO
-	memset(&area->sig_data[sizeof(public_key) + sizeof(calculated_hash)], 0, 1);
+    memset(&area->sig_data[sizeof(public_key) + sizeof(calculated_hash)], 0, 1);
 #endif
 
-	/* Copying user application code */
-	fdbin = open(APP_TEXT_BINARY,O_RDONLY);
-	area->app_text_size = read(fdbin, area->app_text, APP_MAX_SIZE);
-	close(fdbin);
-	/* Copying user application data */
-	fdbin = open(APP_DATA_BINARY,O_RDONLY);
-	area->app_data_size = read(fdbin, area->app_data, APP_MAX_SIZE);
-	close(fdbin);
+    /* Copying user application code */
+    fdbin = open(APP_TEXT_BINARY,O_RDONLY);
+    area->app_text_size = read(fdbin, area->app_text, APP_MAX_SIZE);
+    close(fdbin);
+    /* Copying user application data */
+    fdbin = open(APP_DATA_BINARY,O_RDONLY);
+    area->app_data_size = read(fdbin, area->app_data, APP_MAX_SIZE);
+    close(fdbin);
 }
 
 static void print_secure(int fd)
@@ -67,59 +67,59 @@ static void print_secure(int fd)
 
 int main(void)
 {
-	mram_t *area1, *area2;
-	pim_params_t params;
-	int retval;
-	int fdpu, fdbin, readb, cnt =10;
+    mram_t *area1, *area2;
+    pim_params_t params;
+    int retval;
+    int fdpu, fdbin, readb, cnt =10;
 
-	// Open pim node
-	fdpu = open("/dev/pim", O_RDWR);
-	if (fdpu < 0) {
-		perror("Failed to open /dev/pim device node");
-		exit(EXIT_FAILURE);
-	}
+    // Open pim node
+    fdpu = open("/dev/pim", O_RDWR);
+    if (fdpu < 0) {
+        perror("Failed to open /dev/pim device node");
+        exit(EXIT_FAILURE);
+    }
 
-	// Try to get magic memory
-	void *va = mmap(NULL, DPU_CLUSTER_MEMORY_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fdpu, 0);
-	if (va == MAP_FAILED ) {
-		perror("Failed to get DPU memory");
-		exit(EXIT_FAILURE);
-	} else {
-		area1 = (mram_t*)(va);
-		area2 = (mram_t*)((unsigned long)va+(DPU_CLUSTER_MEMORY_SIZE/2));
-		prepare_data(area1);
-		prepare_data(area2);
-	}
+    // Try to get magic memory
+    void *va = mmap(NULL, DPU_CLUSTER_MEMORY_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fdpu, 0);
+    if (va == MAP_FAILED ) {
+        perror("Failed to get DPU memory");
+        exit(EXIT_FAILURE);
+    } else {
+        area1 = (mram_t*)(va);
+        area2 = (mram_t*)((unsigned long)va+(DPU_CLUSTER_MEMORY_SIZE/2));
+        prepare_data(area1);
+        prepare_data(area2);
+    }
 
-	// Copy DPU program to MRAM
-	fdbin = open(DPU_BINARY,O_RDONLY);
-	if (fdbin < 0) {
-		perror("Failed to open DPU_BINARY");
-		exit(EXIT_FAILURE);
-	}
+    // Copy DPU program to MRAM
+    fdbin = open(DPU_BINARY,O_RDONLY);
+    if (fdbin < 0) {
+        perror("Failed to open DPU_BINARY");
+        exit(EXIT_FAILURE);
+    }
 
-	readb = read(fdbin, area1->code, (DPU_CLUSTER_MEMORY_SIZE/2));
-	if (readb == 0) {
-		perror("DPU_BINARY is empty");
-		exit(EXIT_FAILURE);
-	}
-	lseek(fdbin, 0, SEEK_SET);
-	read(fdbin, area2->code, readb);
-	close(fdbin);
+    readb = read(fdbin, area1->code, (DPU_CLUSTER_MEMORY_SIZE/2));
+    if (readb == 0) {
+        perror("DPU_BINARY is empty");
+        exit(EXIT_FAILURE);
+    }
+    lseek(fdbin, 0, SEEK_SET);
+    read(fdbin, area2->code, readb);
+    close(fdbin);
 
     // Load and run DPU program
-	params.arg1 = (uint64_t)(area1->code);
-	params.arg2 = (uint64_t)(area2->code);
-	retval = ioctl(fdpu, PIM_IOCTL_LOAD_DPU, &params);
-	if (retval < 0 ) {
-		perror("Failed to control pim");
-		exit(EXIT_FAILURE);
-	} else {
-		printf("Dpu load returned %ld\n", params.ret0);
-	}
+    params.arg1 = (uint64_t)(area1->code);
+    params.arg2 = (uint64_t)(area2->code);
+    retval = ioctl(fdpu, PIM_IOCTL_LOAD_DPU, &params);
+    if (retval < 0 ) {
+        perror("Failed to control pim");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Dpu load returned %ld\n", params.ret0);
+    }
 
-	/* Polling is not working */
-	sleep(20);
+    /* Polling is not working */
+    sleep(20);
     // Poll DPU1
     params.arg1 = (uint64_t)area1;
     do {
@@ -147,10 +147,10 @@ int main(void)
     } while ((params.ret1 == 1) && (cnt > 0));
     printf("Dpu %p status is %ld %ld\n", area2, params.ret0, params.ret1);
 
-	print_secure(fdpu);
-	printf("Verification status %ld %ld\n", area1->verification_status, area2->verification_status);
+    print_secure(fdpu);
+    printf("Verification status %ld %ld\n", area1->verification_status, area2->verification_status);
 
-	// Exit gracefully
-	close(fdpu);
+    // Exit gracefully
+    close(fdpu);
     exit(EXIT_SUCCESS);
 }
